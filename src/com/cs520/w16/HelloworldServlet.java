c518a36b8a7e9b18862f58707320beba2fa591de
abalani <anil.balani6@gmail.com>
2016-02-17 14:03:26 -0800

    hello
    fist commit

---
+++ src/com/cs520/w16/HelloworldServlet.java
@@ -0,0 +1,11 @@
+package com.cs520.w16;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelloworldServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, Pritams");
+	}
+}
